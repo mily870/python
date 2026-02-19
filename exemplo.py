afa9822c7a4162b486451e879452320f85dea4e0
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-02-19 08:17:37 -0300

    a

--- exemplo.py
+++ exemplo.py
@@ -2,3 +2,7 @@ print("hello world")
 import this
 print(7+4)
 print('7'+'4')
+nome=input ('qual é o seu nome')
+idade=input ('qual é a sua idade')
+peso=input ('qual é seu peso')
+print(nome,idade,peso)
