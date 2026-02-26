a77c8ceb54840e6f3efd7f526abe26af83bad42e
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-02-26 09:00:49 -0300

    a

---
+++ exercicio/ex02.py
@@ -0,0 +1,4 @@
+dia=input ('qual é sua data do seu nascimento?')
+mes=input ('qual é seu mês do seu nascimento?')
+ano=input ('qual é o ano do seu nascimento?')
+print('dia: {}  mês: {}  ano: {} '.format (dia, mes, ano))
---
+++ exercicio/ex04.py
@@ -0,0 +1,5 @@
+#tipos de dados
+#int é igual a numeros inteiros
+#float é igual a numeros com virgula
+#bool é para armazenar valores de verdadeiro e falso
+#str armazena texto
