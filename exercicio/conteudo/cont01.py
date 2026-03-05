fb7a4a193a001d592ded8769605baf7bcefba5b2
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-05 08:43:41 -0300

    a

--- exercicio/conteudo/cont01.py
+++ exercicio/conteudo/cont01.py
@@ -1,2 +1,8 @@
 n=input('digite algo')
-print(n.isnumeric())
+print(n.isnumeric()) #para testar número inteiro
+print(n.isalpha()) #para testar letra
+print(n.isalnum()) #testa se é letra ou número
+print(n.isupper()) #verificar se tem letra maiuscula
+print(n.isdecimal()) #verificar se tem numero decimal
+print(n.islower()) #verificar letras minusculas
+print(n.isspace()) #verificar se tem espaço
---
+++ exercicio/conteudo/cont02.py
@@ -0,0 +1,7 @@
+# + é igual a soma 
+# - é igual a subtração
+# * é igual a multiplicação
+# / é igual a divisão
+# ** é igual a exporenciação
+# // é igual a divisão inteira 
+# % é igual o resto da divisão
