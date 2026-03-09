ba7408a616e646304cf3d145ebdd415ba0b08184
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-09 11:47:05 -0300

    a

--- exercicio/conteudo/cont02.py
+++ exercicio/conteudo/cont02.py
@@ -21,3 +21,13 @@ print (5==(2.5*2))
 # 2. potenciacão 
 # 3. multiplicação e divisão
 # 4. soma e subtração
+
+print (5+3*2)
+print (3*5+4**2)
+print (3*(5+4)**2)
+
+#pow (x,y)
+
+print (pow(3,4)) #potenciação 
+print (27**(1/3))
+print ('='*5)
