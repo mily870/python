b2564d6f8e718a2fdb18656effc0de978c29d3ea
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-10 08:58:58 -0300

    a

--- exercicio/ex09.py
+++ exercicio/ex09.py
@@ -1,5 +1,5 @@
-nota1 = int (input('digite sua nota'))
-nota2 = int  (input('digite sua outra nota'))
+nota1 = float (input('digite sua nota'))
+nota2 = float (input('digite sua outra nota'))
 media1 = nota1+nota2
 media2 = media1/2
 print ('as notas são {} e {} e a média é {}'.format (nota1, nota2, media2))
