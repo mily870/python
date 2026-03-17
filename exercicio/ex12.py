6e04c8aff58eed9a6be485d8a579a034ad4f7a28
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-17 12:02:12 -0300

    A

---
+++ exercicio/ex12.py
@@ -0,0 +1,4 @@
+carte=float(input('quantos dinheiro na carteira você tem?'))
+dolar=5.38
+conve=carte*dolar
+print('você tem {} reais em dinheiro, convertendo isso para dólar você tem {}'.format(carte, conve))
