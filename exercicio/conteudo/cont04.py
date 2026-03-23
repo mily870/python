b56f5307d50a2a2387209851bf9b15bb8ff2d56d
Kamily Matias <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-23 12:26:11 -0300

    Refactor square root calculation with specific imports
    Updated the code to use specific math functions and added floor and ceil calculations.

--- exercicio/conteudo/cont04.py
+++ exercicio/conteudo/cont04.py
@@ -1,4 +1,11 @@
-import math 
-num = int (input('digite um número'))
-raiz = math.sqrt (num)
-print ('a raiz de {} é igual a {}'.format(num, raiz))
+#import math 
+#num = int (input('digite um número'))
+#raiz = math.sqrt (num)
+#print ('a raiz de {} é igual a {}'.format(num, math.ceil (raiz)))
+#print ('a raiz de {} é igual a {}'.format(num, math.floor (raiz)))
+
+#importando funcionalidades especificas
+
+from math import sqrt, floor 
+num = int (input('digite um número')) 
+raiz = sqrt (num)
