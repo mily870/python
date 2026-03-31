3d712226c2a88883ab423dd4cef8a8f801910946
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-03-31 12:22:59 -0300

    h

---
+++ exercicio/ex021.py
@@ -0,0 +1,8 @@
+from random import choice
+n1 = str(input('Primeiro aluno(a) : '))
+n2 = str(input('Segundo aluno(a): '))
+n3 = str(input('Terceiro aluno(a): '))
+n4 = str(input('Quarto aluno(a): '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
