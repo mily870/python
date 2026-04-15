a9a57f8ad9f9baf0be879edfb60267a3644181a7
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-04-15 09:18:08 -0300

    a

--- exemplo.py
+++ exemplo.py
@@ -1,8 +1,12 @@
-print("hello world")
-import this
-print(7+4)
-print('14'+'57')
-nome=input ('qual é o seu nome')
-idade=input ('qual é a sua idade')
-peso=input ('qual é seu peso')
-print(nome,idade,peso)
+import random
+aluno1 = input("Primeiro aluno: ")
+aluno2 = input("Segundo aluno: ")
+aluno3 = input("Terceiro aluno: ")
+lista = [aluno1, aluno2, aluno3]
+escolhido = random.choice(lista)
+print(f"O aluno escolhido foi: {escolhido}")
+
+
+
+
+
