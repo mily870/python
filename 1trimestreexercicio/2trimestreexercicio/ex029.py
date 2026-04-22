09e57a8d56862f1c325eed3ad345e0874a299f5c
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-04-22 09:47:00 -0300

    a

---
+++ 1trimestreexercicio/2trimestreexercicio/ex029.py
@@ -0,0 +1,5 @@
+nome = str (input('digite seu nome completo')).strip()
+dividido = nome.split()
+print ('muito prazer em te conhecer')
+print ('seu primeiro nome é {}'.format(dividido[0]))
+print ('seu ultimo nome é {}'.format(dividido[len(dividido)-1]))
