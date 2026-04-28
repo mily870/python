fb375f57007a6d330ddbe4a4f6a5d53e24978ca9
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-04-28 09:48:03 -0300

    a

--- 1trimestreexercicio/conteudo/cont08.py
+++ 1trimestreexercicio/conteudo/cont08.py
@@ -1,6 +1,25 @@
-tempo = int (input('quantos anos tem seu carro?'))
-if tempo <=3:
-    print ('carro novo')
+#tempo = int (input('quantos anos tem seu carro?'))
+#if tempo <=3:
+#print ('carro novo')
+#else:
+    #print ('carro velho')
+#print ('fim')
+
+#tempo = int (input('quantos anos tem o seu carro?'))
+#print ('carro novo'if tempo <=3 else'carro velho')
+
+#nome = str(input('qual é o seu nome?'))
+#if nome == 'kamily':
+    #print ('que nome lindo vc tem!')
+#else: 
+    #print ('o seu nome é tão normal')
+#print ('bom dia! {}'.format (nome))
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2)/2
+print("A sua média foi {:.1f}".format(m))
+if m >= 6.0:
+  print("A sua média foi boal Parabéns")
 else:
-    print ('carro velho')
-print ('fim')
+  print("Sua média foi ruim! Estude mais")
