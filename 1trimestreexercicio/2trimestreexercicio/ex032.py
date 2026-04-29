659ed259cf6a0efce6b25864c6df08f6e616c1f4
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-04-29 08:54:20 -0300

    a

---
+++ 1trimestreexercicio/2trimestreexercicio/ex032.py
@@ -0,0 +1,6 @@
+numero = int(input('digite um número inteiro'))
+if numero % 2 == 0:
+    print('O número {} é par'.format(numero))
+else:
+    print('O número {} é impar'.format(numero))
+    print (numero)
