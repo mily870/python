eb82b40add79abeb04c26dbbafd71aeca3d02ea5
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-05-06 09:04:49 -0300

    a

---
+++ 1trimestreexercicio/2trimestreexercicio/ex037.py
@@ -0,0 +1,7 @@
+a = float(input("Primeiro segmento"))
+b = float(input("Segundo segmento"))
+c = float(input("Terceiro segmento "))
+if a<b+c and b<a+c and c<a+b:
+ print("Os segmentos acima podem formar um triangulo")
+else:
+ print("os segmentos acima não podem formar um triangulo")
