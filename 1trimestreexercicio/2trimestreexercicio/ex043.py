29ba8de8d489eada17c95c4db20a8badfd08fa63
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-05-18 09:00:04 -0300

    a

---
+++ 1trimestreexercicio/2trimestreexercicio/ex043.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+nascimento = int(input('Ano de Nascimento: '))
+atual = date.today().year
+idade = atual - nascimento
+
+print(f'O atleta tem {idade} anos.')
+
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+else:
+    print('Classificação: MASTER')
---
+++ 1trimestreexercicio/conteudo/cont10.py
@@ -0,0 +1,16 @@
+for c in range (0,6):
+    print('oi')
+print('fim')
+
+for c in range (0,6):
+    print(c)
+print('fim')
+
+for c in range (6,-1,-1):
+    print(c)
+print('fim')
+
+for c in range (0,6,2):
+    print(c)
+print('fim')
+
