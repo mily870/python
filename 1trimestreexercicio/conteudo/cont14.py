34616f4d591123d2b1ba852ab659e828f2af8216
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-08-24 08:10:02 -0300

    a

--- 1trimestreexercicio/conteudo/cont14.py
+++ 1trimestreexercicio/conteudo/cont14.py
@@ -1,7 +1,20 @@
-num = [2,5,9,1]
-num [2] = 3
-num . append(7)
-num . sort(reverse=True)
-num . insert(2,2)
-num . remove(2)
-print(num)
+# num = [2,5,9,1]
+# num [2] = 3
+# num . append(7)
+# num . sort(reverse=True)
+# num . insert(2,2)
+# num . remove(2)
+# if 4 in num :
+#     num . remove(4)
+# else :
+#     print ('não achei o numero 4')
+# num . pop(2)
+# print(num)
+
+valores = []
+valores . append(5)
+valores . append(9)
+valores . append(4)
+print(valores)
+for c, v in enumerate (valores):
+    print(f'na posição {c} eu achei o valor {v} ', end='')
