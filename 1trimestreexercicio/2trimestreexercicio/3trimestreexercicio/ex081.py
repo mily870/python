eec6920afe6184859d4aae339e8a8c249e85ff4c
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-08-31 07:36:46 -0300

    a

--- 1trimestreexercicio/2trimestreexercicio/3trimestreexercicio/ex081.py
+++ 1trimestreexercicio/2trimestreexercicio/3trimestreexercicio/ex081.py
@@ -1,8 +1,12 @@
-num = [2,5,9,1]
-num . append(7)
-if 4 in num :
-    num . remove(4)
-else :
-    print ('não achei o numero 4')
-num . pop(2)
-print(num)
+valor = []
+continuar = 's'
+while continuar == 's':
+   num =int(input('Digite um valor'))
+if num in valor:
+  print('valor duplicado! não vou adicionar')
+else:
+  valor.append(num)
+print('Valor adicionado com sucesso')
+continuar = str(input('Quer continuar?')).lower().strip()[0]
+valor.sort()
+print(f'Você digitou os valores {valor}')
