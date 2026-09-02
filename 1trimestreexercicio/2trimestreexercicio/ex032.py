e23195cbff11d4e00f5f800d1f2b6cb605ce8476
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-02 07:38:00 -0300

    kjb

--- 1trimestreexercicio/2trimestreexercicio/ex032.py
+++ 1trimestreexercicio/2trimestreexercicio/ex032.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 numero = int(input('digite um número inteiro'))
 if numero % 2 == 0:
     print('O número {} é par'.format(numero))
 else:
     print('O número {} é impar'.format(numero))
+=======
+
+>>>>>>> Stashed changes
