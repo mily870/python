2d329f49b672d14448c03eaccac7ce9e7f9f6ecb
kamily <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-02 07:37:25 -0300

    jhh

---
+++ 1trimestreexercicio/2trimestreexercicio/3trimestreexercicio/ex085.py
@@ -0,0 +1,11 @@
+expr = str(input('digite uma expressão: '))
+esquerda = direita = 0
+for simb in expr:
+    if simb == '(':
+        esquerda +=1
+    elif simb == ')':
+        direita += 1
+if esquerda == direita:
+    print('A expressão esta correta')
+else:
+    print('A expressão não está correta')
