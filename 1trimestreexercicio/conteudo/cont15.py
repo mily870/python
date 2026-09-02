d47110937b7bd69fd159088fb52e9f3d5bf69be7
Victor-Dev8192 <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-02 08:11:10 -0300

    a

---
+++ 1trimestreexercicio/conteudo/cont15.py
@@ -0,0 +1,11 @@
+teste = []
+teste . append('kamily')
+teste . append(17)
+galera = []
+#galera . append(teste)
+galera . append(teste [:])
+teste [0] = 'maria'
+teste [1] = '22'
+galera . append(teste [:])
+print(teste)
+print(galera)
