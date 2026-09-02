47cfe8a4f720df54e6f86a26d2c2b48c4263032a
Victor-Dev8192 <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-02 07:52:23 -0300

    jhji

--- 1trimestreexercicio/2trimestreexercicio/ex079.py
+++ 1trimestreexercicio/2trimestreexercicio/ex079.py
@@ -5,4 +5,4 @@ for p in palavras:
     print(f'\nNa palavra {p} temos ', end = '')
     for letra in p:
         if letra.lower() in 'aeiou':
-            print(letra, end = '')
+            print(letra, end = '')jjj
