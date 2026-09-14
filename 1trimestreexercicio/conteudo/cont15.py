9451a933cde383421af02d04a80414607263222f
Victor-Dev8192 <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-14 09:51:09 -0300

    a

--- 1trimestreexercicio/conteudo/cont15.py
+++ 1trimestreexercicio/conteudo/cont15.py
@@ -25,3 +25,12 @@ for c in range (0,3):
     galera.append (dado[:])
     dado.clear()
 print(galera)
+
+for p in galera:
+    if p[1] >=18:
+        print(f'{p[0]} é maior de idade')
+        tomai += 1
+    else:
+        print (f'{p[0]} é menor de idade')
+        tomen += 1
+print(f'Temos (tomai) maiores de idade e (tomen) menores de idade')
