646e824690f663f8c9b7741fa5bbad0d1b838f0e
Victor-Dev8192 <kamily.sonntag.matias@escola.pr.gov.br>
2026-09-15 12:23:57 -0300

    a

--- 1trimestreexercicio/2trimestreexercicio/3trimestreexercicio/ex086.py
+++ 1trimestreexercicio/2trimestreexercicio/3trimestreexercicio/ex086.py
@@ -5,11 +5,20 @@ while True :
     temp . append(str(input('nome')))
     temp . append(float(input('peso')))
     if len (princ) == 0:
-        mai = men = temp = [1]
+        mai = men = temp [1]
     else:
         if temp [1] > mai:
            mai = temp [1]
         if temp [1] < men:
            men = temp [1]
-        princ . append (temp[:])
-        temp . clear ()
+    princ . append (temp[:])
+    temp . clear ()
+    resp = str(input('quer continuar?')) . lower() . strip()[0]
+    if resp == 'n':
+            break
+print (f'os dados foram {princ}')
+print (f'vc cadastrou {len(princ)} pessoas')
+print (f'o menor peso foi de {men} kg. peso de', end='')
+for p in princ:
+    if p[1] == men:
+        print (f'[{p[0]}]' , end='')
